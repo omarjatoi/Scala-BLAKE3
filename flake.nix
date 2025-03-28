4282d9d27aeee993f5e101ed94a14f3ec887f0b0
Omar Jatoi <git@omarjatoi.com>
2025-03-28 09:24:01 -0400

    feat: add nix development shell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Pure Scala port of the BLAKE3 reference implementation";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        javaVersion = 17;
+        jdk = pkgs."jdk${toString javaVersion}";
+        sbt = pkgs.sbt.override { jre = jdk; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            jdk
+            sbt
+            pkgs.metals
+            pkgs.scala-next
+            pkgs.scalafmt
+            pkgs.scalafix
+          ];
+
+          shellHook = ''
+            echo "Scala-BLAKE3 development environment"
+            echo "JDK version: ${toString javaVersion}"
+            echo "Using sbt: $(sbt --version | head -n 1)"
+          '';
+        };
+      });
+}
